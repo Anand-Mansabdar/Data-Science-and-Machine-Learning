c51654a0609df806562dd752ef263170762bb8d6
Anand-Mansabdar <dummystudy040105@gmail.com>
2025-11-09 13:28:07 +0530

    Working with HTTP get and post methods

---
+++ 15 - Flask Basics/15.3 - httpVerbs.py
@@ -0,0 +1,27 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def welcome():
+  return "<html><h1>Hello World!!</h1></html>"
+
+@app.route('/contact', methods=['GET'])
+def contact():
+  return render_template('contact.html')
+
+@app.route('/about')
+def about():
+  return render_template('about.html')
+
+@app.route('/form', methods=['GET','POST'])
+def form():
+  if request.method == 'POST':
+    name = request.form['name']
+    return f'Hello {name}'
+  return render_template('form.html')
+
+if __name__ == "__main__":
+  app.run(debug=True)
+  
